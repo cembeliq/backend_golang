8dffda1ec766ad1a9daedf063c905ad41bd6f446
agro cembeliq <cembeliq@gmail.com>
2021-02-15 15:15:50 +0700

    add viper configuration

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-playground/validator/v10 v10.4.1
 	github.com/labstack/echo/v4 v4.2.0
+	github.com/spf13/viper v1.7.1
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 )
--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "cembeliq_app/routes"
+import (
+	"cembeliq_app/config"
+	"cembeliq_app/routes"
+	"log"
+)
 
 func main() {
+	env := config.EnvVariable("ENVIRONMENT")
+
+	log.Println("ENVIRONMENT TYPE: " + env)
 	r := routes.Init()
 
-	r.Logger.Fatal(r.Start(":9000"))
+	r.Logger.Fatal(r.Start(config.EnvVariable("APP_SERVER_PORT")))
 }
